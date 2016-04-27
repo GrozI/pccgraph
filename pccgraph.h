3a28f8cd53bfaf4cf888d0ee53125c6ff3eb0e29
GrozI <irenee.groz@gmail.com>
2016-04-27 17:44:36 +0200

    Update pccgraph.h

--- pccgraph.h
+++ pccgraph.h
@@ -11,41 +11,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-/*struct liste {
- Arc arc;
- struct liste *suiv;};
- typedef struct liste * Liste;
+
  
- Liste creer_liste(void);
- int est_vide(Liste L);
- void visualiser_liste(Liste L);
- Liste ajout_tete(CARTE c,Liste L);
- Liste supprimer_tete(Liste L);
- Liste ajout_queue(CARTE c,Liste L);
- Liste supprimen(int n, Liste L);
- Liste concat(Liste L1, Liste L2);
- Liste copie(Liste L);
- */
+
+
 
 
 typedef struct {
     unsigned int nX;
     unsigned int nA;
-    Sommets * sommets;
-} * graphe;
+    Sommets* sommets;
+}* graphe;
 typedef struct {
     double poids;
-    unsigned int ;
-    Liste * arc;
+    char station;
+    unsigned int No;
+    Liste* arc;
     
-} * Sommets;
+} Sommets;
 typedef struct {
     double poids;
+    char line;
     unsigned int pred;
     unsigned int dest;
-} * Arc;
+} Arc;
+struct liste {
+ Arc arc;
+ struct liste *suiv;};
+ typedef struct liste * Liste;
+
+Liste creer_liste(void);
+int est_vide(Liste L);
+void visualiser_liste(Liste L);
+Liste ajout_tete(CARTE c,Liste L);
+Liste supprimer_tete(Liste L);
+Liste ajout_queue(CARTE c,Liste L);
+
 
 graphe nouveau_graphe(unsigned int nX,unsigned int nA);
+void visualiser_sommets(Sommets* s);
 void affiche_graphe(graphe g);
 void detruit_graphe(graphe g);
 void graphe_ecrit_nA(graphe g, unsigned int nA);
@@ -56,7 +60,7 @@ void graphe_ecrit_poids(graphe g, unsigned int u, double val);
 double graphe_lit_poids(graphe g, unsigned int u);
 void graphe_ecrit_poids_arc(graphe g, unsigned int u, unsigned int v, double val);
 double graphe_lit_poids_arc(graphe g, unsigned int u, unsigned int v);
-graphe lit_graphe(char * fichier);
 void graphe_ajoute_arc(graphe g, unsigned int u, unsigned int v, double val);
+graphe lit_graphe(char * fichier);
 double graphe_pcc(graphe g, unsigned int u, unsigned int v);
 #endif /* pccgraph_h */
